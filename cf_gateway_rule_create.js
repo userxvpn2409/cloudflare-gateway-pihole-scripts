bc9d6cea0b1690b69db905c770b6f65235c12946
userxvpn2409 <145556125+userxvpn2409@users.noreply.github.com>
2023-09-22 01:36:43 +0700

    Update cf_gateway_rule_create.js

--- cf_gateway_rule_create.js
+++ cf_gateway_rule_create.js
@@ -2,7 +2,7 @@ import { createZeroTrustRule, getZeroTrustLists } from "./lib/api.js";
 
 const { result: lists } = await getZeroTrustLists();
 const wirefilterExpression = lists.reduce((previous, current) => {
-  if (!current.name.startsWith("")) return previous;
+  if (!current.name.startsWith("CGPS List")) return previous;
 
   return `${previous} any(dns.domains[*] in \$${current.id}) or `;
 }, "");
